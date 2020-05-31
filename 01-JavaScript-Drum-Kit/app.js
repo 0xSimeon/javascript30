9c31a561a1bf263f750d1fcc2ad3df0bede7d1d8
simeon4real <simeon.udoh45@gmail.com>
2020-05-31 18:32:41 +0100

    Added Click functionality :zap: , nice background :lipstick:  and footer :pencil

--- 01-JavaScript-Drum-Kit/app.js
+++ 01-JavaScript-Drum-Kit/app.js
@@ -1,20 +1,35 @@
-window.addEventListener(
-    'keydown', function(e) {
-        const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-        const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-        if(!audio) return; // stop the function from running 
-        audio.currentTime = 0; // Rewind to the start
-        audio.play();
-        key.classList.add('playing');
-    } 
-); 
+const keys = document.querySelectorAll(".key");
 
-function removeTransition(e) {
-    if(e.propertyName !== 'transform') return; // skip if its not a transform
-    this.classList.remove('playing'); 
-
+function playSound(e) {
+	const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+	const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+	if (!audio) return; // stop the function from running
+	audio.currentTime = 0; // Rewind to the start
+	audio.play();
+	key.classList.add("playing");
 }
 
-const keys = document.querySelectorAll('.key');
-keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+function playSoundClick(e) {
+    const currentElem = e.target.parentElement; 
+    const key = currentElem.dataset.key;
+    const keyElem = e.target.parentElement; 
+    const audio = document.querySelector(`audio[data-key="${key}"]`);
+    if (!audio) return;
+    audio.currentTime = 0; // Rewind to the start
+	audio.play();
+	keyElem.classList.add("playing");
+}
 
+function removeTransition(e) {
+	if (e.propertyName !== "transform") return; // skip if its not a transform
+	this.classList.remove("playing");
+}
+
+// Event Listeners
+window.addEventListener("keydown", playSound);
+window.addEventListener("click", playSoundClick);
+keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
+
+// Update copyright year automatically
+const date = new Date().getFullYear();
+document.getElementById('year').textContent = date; 
